e3542d667dad96cb88c3a6a80fdfe8d9f566aa46
Brandon Miller <blmiller0809@gmail.com>
2021-02-18 17:45:14 -0600

    config object

--- app.py
+++ app.py
@@ -2,30 +2,32 @@ import requests
 import json
 import time
 from playsound import playsound
-from config import Config
+from config import *
 
-base_url = Config.base_url
 
-def app(abr, test, team_id, stream_delay):
+def watch_game():
 
-	def refresh_data(test, team_id):
-		data = config.get_data(test, team_id)
+	test = game.test
+	abr = game.abr
+	team_id = game.team_id
+	stream_delay = game.stream_delay
+	data = game.data
+
+	def refresh_data():
+		data = game.get_data(test, team_id)
 		return data
 
 
-	def celebration(abr):
+	def celebration():
 		"""celebration when the your team scores."""
 		print(f"\n\n{abr} SCORED!!!\n\n")
 		playsound(f"{abr}.mp3")
 
 
-	def win(abr):
+	def win():
 		"""celebration to play when your team wins"""
 		print(f"\n\n{abr} WINS!!!\n\n")
 		playsound(f"{abr}_win.mp3")
-
-
-	data = refresh_data(test, team_id)
 
 	home = False
 	away = False
@@ -34,35 +36,39 @@ def app(abr, test, team_id, stream_delay):
 	"""Here the fun begins...if there is a game, check status. If status is 'Live'
 	then we keep checking the score."""
 	while games > 0:
-		data = refresh_data(test, team_id)
+		data = refresh_data()
 		game_status = data['dates'][0]['games'][0]['status']['abstractGameState']
 
 		if game_status == "Preview":
 			while game_status == "Preview":
 				data = refresh_data(test, team_id)
+				game_status = data['dates'][0]['games'][0]['status']['abstractGameState']
 				print("Waiting for puck drop.")
 				time.sleep(1)
 				print("Waiting for puck drop..")
 				time.sleep(1)
 				print("Waiting for puck drop...")
-				time.sleep(4)
+				time.sleep(10)
 				print("\n\nCHECKING STATUS\n")
 
 
-		if game_status == "Live":
-			i = 0	
+		elif game_status == "Live":
 
 			away_teamId = data['dates'][0]['games'][0]['teams']['away']['team']['id']
 			home_teamId = data['dates'][0]['games'][0]['teams']['home']['team']['id']
 
 			if home_teamId == team_id: # set home or away for the team you picked
 				home = True
+				print(f"----------\n----------\nThe puck has dropped at {team.venue}!\n")
 			if away_teamId == team_id:
 				away = True
+				print("----------\n----------\nThe puck has dropped!\n")
 
-			while True:
+			i = 0	
+
+			while game_status == "Live":
 				"""Continous loop to check game score, play cellys"""
-				data = refresh_data(test, team_id)
+				data = refresh_data()
 
 				game_status = data['dates'][0]['games'][0]['status']['abstractGameState']
 
@@ -74,19 +80,18 @@ def app(abr, test, team_id, stream_delay):
 					if away:
 						if away_score != away_scoreLast:
 							time.sleep(stream_delay) # adjust to line up with your stream
-							celebration(abr)
+							celebration()
 						if home_score != home_scoreLast:
 							time.sleep(stream_delay)
 							print("BAD GUYS SCORED")
 					if home:
 						if home_score != home_scoreLast:
 							time.sleep(stream_delay)
-							celebration(abr)
+							celebration()
 						if away_score != away_scoreLast:
 							time.sleep(stream_delay)
 							print("BAD GUYS SCORED")
 				else:
-					print("\n\nGAME START!\n\n")
 					i += 1 # set i equal to 1 on first iteration
 
 				# set last loop's scores to compare against
@@ -94,23 +99,26 @@ def app(abr, test, team_id, stream_delay):
 				home_scoreLast = home_score
 				print(home_score, away_score)
 
-				if game_status == "Final":
-					if away:
-						if away_score > home_score:
-							time.sleep(stream_delay)
-							win(abr) # PLAY GLORIA!
-					if home:
-						if home_score > away_score:
-							time.sleep(stream_delay)
-							win(abr)
-					print("GAME OVER")
-					break
-
-				time.sleep(2) # request every x seconds
+				time.sleep(1.5) # request every x seconds
 				# END WHILE TRUE LOOP #
 
 		elif game_status == "Final":
-			print("Today's game has already ended.")
+			if away:
+				if away_score > home_score:
+					time.sleep(stream_delay)
+					win(abr) # PLAY GLORIA!
+				else:
+					print("Sorry for your loss.")
+			elif home:
+				if home_score > away_score:
+					time.sleep(stream_delay)
+					win(abr)
+				else:
+					print("Sorry for your loss.")
+			else:
+				print("Today's game is over.")
+			
+			break
 
 		else: 
 			print("Error involving game_status. Please try again.")
@@ -121,12 +129,7 @@ def app(abr, test, team_id, stream_delay):
 	
 if __name__ == "__main__":
 	
-	abr = input("Please enter the abbreviation of your favorite team! ").upper()
-	config = Config(abr)
+	game = Config()
+	team = Team(game.team_id)
 
-	team_id = config.get_team(abr)
-	print(f"Team ID is: {team_id}")
-	test = config.test()
-	stream_delay = config.stream_delay(test)
-
-	app(abr, test, team_id, stream_delay)
+	watch_game()
--- config.py
+++ config.py
@@ -1,56 +1,114 @@
 import requests
 import json
+import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
 
 class Config:
 
-	base_url = "https://statsapi.web.nhl.com/api/v1/"
+	base_url = "https://statsapi.web.nhl.com"
 
-	def __init__(self, abr):
-		self.abr = abr
-
+	def __init__(self):
+		self.test()
+		self.get_abr()
+		self.get_team(self.abr)
+		self.get_data(self.test, self.team_id)
+		self.stream_delay(self.test)
 
 	def test(self):
-		test = ''
-		while test == '':
-			testing = input("Run in test mode?(Y/N) ").upper()
-			
-			if testing == "Y":
-				test = True # set to True to test with local json files
-			elif testing == "N":
-				test = False
-			else:
-				print("Please enter a valid test input.")
 
-		return test
+		testing = input("TESTING?\nY for yes\nEnter for no").upper()
+		
+		if testing == "Y":
+			self.test = True # set to True to test with local json files
+		else:
+			self.test = False
+
+		print("\n")
+
+
+	def get_abr(self):
+		self.abr = input("Please enter the abbreviation of your favorite team!\n").upper()
+		print("\n")
+
 
 
 	def get_team(self, abr):
-		r = requests.get(Config.base_url + "teams")
+		r = requests.get(self.base_url + "/api/v1/teams")
 		data = r.json()
-		
+			
 		for team in data['teams']:
 			if team['abbreviation'] == abr:
-				team_id = team['id']
+				self.team_id = team['id']
 
-		return team_id
+		print(f"Team ID set to {self.team_id}...")
 
 
 	def stream_delay(self, test):
 		if test:
-			stream_delay = 0
+			self.stream_delay = 0
 		else:
-			stream_delay = 34
+			self.stream_delay = 34
 
-		return stream_delay
+		print(f"Stream delay set to {self.stream_delay}...\n")
 
 
 	def get_data(self, test, team_id):
 		if test:
 			with open('sched.json') as f: # data from file for testing purposes
-				data = json.load(f)
+				self.data = json.load(f)
 		else:
-			r = requests.get(Config.base_url + f"schedule?teamId={team_id}")
-			data = r.json()
+			r = requests.get(self.base_url + f"/api/v1/schedule?teamId={team_id}&expand=schedule.linescore")
+			self.data = r.json()
 
-		return data
+		return self.data
+
+
+
+
+class Team:
+
+	base_url = "https://statsapi.web.nhl.com"
+
+	def __init__(self, team_id):
+		self.team_id = team_id
+		self.get_team_info(self.team_id)
+		self.get_team_players(self.team_id)
+
+	def get_team_info(self, team_id):
+		r = requests.get(self.base_url + f"/api/v1/teams/{team_id}")
+		data = r.json()
+
+		self.team_name = data['teams'][0]['name']
+		self.team_short_name = data['teams'][0]['franchise']['teamName']
+		self.city = data['teams'][0]['venue']['city']
+		self.venue = data['teams'][0]['venue']['name']
+
+		self.team_info = {
+			"teamName": self.team_name,
+			"nickname": self.team_short_name,
+			"city": self.city,
+			"venue": self.venue,
+			}
+
+
+	def get_team_players(self, team_id):
+		r = requests.get(self.base_url + f"/api/v1/teams/{team_id}/roster")
+		data = r.json()
+		self.roster = data['roster']
+
+		numbers = []
+		names = []
+
+		x = 0
+
+		for p in self.roster:
+			p = self.roster[x]['jerseyNumber']
+			numbers.append(p)
+
+			p = self.roster[x]['person']['fullName']
+			names.append(p)
+
+			x += 1
+
+		self.players = dict(zip(numbers, names))
